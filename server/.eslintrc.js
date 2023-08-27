5745c9d023ccbf2433630d07c8bf1fd206d95ace
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2023-08-27 19:22:19 +0300

    server: set up prettier & eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,21 +5,50 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'security',
+    'sonarjs',
+    'unicorn'
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:security/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended'
   ],
+  //
   root: true,
   env: {
     node: true,
     jest: true,
+    es2022: true,
+    browser: false,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-console': 'warn',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/prefer-top-level-await': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-array-reduce': 'warn',
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreUrls: true
+      }
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/ban-ts-comment': 'warn'
   },
+  overrides: []
 };
