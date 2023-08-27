435583b43f322c2fbd6a77e9ec52eae2924acbcd
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2023-08-27 18:53:00 +0300

    server(modules): init Database module

---
+++ server/src/database/entities/index.ts
@@ -0,0 +1,5 @@
+import { UserEntity } from '@entities/user';
+import { AccessTokenEntity } from '@entities/accessToken';
+import { AdminEntity } from '@entities/admin';
+
+export const ALL_DB_ENTITIES = [UserEntity, AccessTokenEntity, AdminEntity];
---
+++ server/src/modules/database/database.module.ts
@@ -0,0 +1,28 @@
+/*external modules*/
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+/*@entities*/
+import { ALL_DB_ENTITIES } from '@entities/index';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          url: configService.getOrThrow('postgres.url'),
+          entities: ALL_DB_ENTITIES,
+          logging: true,
+          synchronize: configService.getOrThrow('database.syncEntities'),
+          autoLoadEntities: configService.getOrThrow(
+            'database.autoLoadEntities',
+          ),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
