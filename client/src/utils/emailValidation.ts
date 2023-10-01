280809bb45c9100ad08c829c453eedad74cf2bdb
serhii-hys <sergey.donchenko@hys-enterprise.com>
2023-10-01 15:24:46 +0300

    feat: add prettier and linter

--- client/src/utils/emailValidation.ts
+++ client/src/utils/emailValidation.ts
@@ -1,1 +1,2 @@
-export const isEmailValid = (email: string): boolean => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+export const isEmailValid = (email: string): boolean =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
