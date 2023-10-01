c40aa0fff5c14099178e5a1cd0b669bf3e2bec30
serhii-hys <sergey.donchenko@hys-enterprise.com>
2023-10-01 15:24:46 +0300

    feat: add prettier and linter

--- client/customTemplate.js
+++ client/customTemplate.js
@@ -1,5 +1,5 @@
 module.exports = (dts, { classes }) => {
-    return Object.keys(classes)
-      .map((key) => `export const ${key}: string`)
-      .join("\n");
-  };
+  return Object.keys(classes)
+    .map(key => `export const ${key}: string`)
+    .join('\n');
+};
