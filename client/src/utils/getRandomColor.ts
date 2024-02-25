e0d93562878d11966ab48c99f3804468fd3440b1
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2024-02-25 18:30:14 +0200

    client(utils): refactor

--- client/src/utils/getRandomColor.ts
+++ client/src/utils/getRandomColor.ts
@@ -1,10 +1,12 @@
 export function generateRandomColorExcludingWhite() {
   const letters = '0123456789ABCDEF';
+
   let color = '#';
   do {
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
   } while (color === '#FFFFFF');
+
   return color;
 }
